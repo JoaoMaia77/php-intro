84fe5852154b59ed242509c87ee023f469111225
João Victor Maia <jv.maia.almeida@gmail.com>
2022-04-11 18:58:32 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+/*
+$categorias = [];
+$categorias[] = 'infantil';
+$categorias[] = 'adolecentes';
+$categorias[] = 'adulto';
+$categorias[] = 'idoso';
+// print_r($categorias)
+
+$nome = 'Eduardo';
+$idade = 25;
+
+//var_dump($nome);
+//var_dump($idade);
+
+if($idade >= 6 && $idade <= 12){
+    for($i = 0; $i <= count($categorias); $i++){
+        if ($categorias[$i] == 'infantil'){
+            echo "Nadador $nome compete na catagoria infantil";
+        }
+    }
+
+}else if($idade>= 13 && $idade <=18){
+    for($i = 0; $i <= count($categorias); $i++){
+        if ($categorias[$i] == 'adolecente'){
+            echo "Nadador $nome compete na catagoria adolecente";
+        }
+    }
+}else if($idade > 18 && $idade <= 50 ){
+for ($i = 0; $i <= count($categorias); $i++){
+    if ($categorias[$i] == 'adulto'){
+        echo "Nadador $nome compete na categoria adulto";
+        }
+    }
+}else{
+    for($i = 0; $i <= count($categorias); $i++){
+        if ($categorias[$i] == 'idoso'){
+            echo "Nadador $nome compete na catagoria idoso";
+        }
+    }
+
+}
+*/
+
+$preco = [];
+$preco[] = 'barato';
+$preco[] = 'intermediario';
+$preco[] = 'caro';
+
+$nome = 'Camisa';
+$valor = 100;
+
+if($valor >= 1 && $valor <= 25){
+    for($i = 0; $i <= count($preco); $i++){
+        if ($preco[$i] == 'barato'){
+            echo "O produto: $nome é barato.";
+        }
+    }
+}else if($valor > 25 && $valor <= 50){
+    for($i = 0; $i <= count($preco); $i++){
+        if($preco[$i] == 'intermediario'){
+            echo "O produto: $nome é intermediario.";
+        }
+    }
+}else{
+    for($i = 0; $i <= count($preco); $i++){
+        if($preco[$i] == 'caro'){
+            echo "O produto: $nome é caro.";
+        }
+    }
+}
+
+
+
